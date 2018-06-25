24cd330640b23676b54776e0a26622897e5758f7
MARCELO <marcelofabiano@msn.com>
2018-06-25 17:11:53 -0300

    Estruturação de cadastro com passagem de parâmetros;

--- Conselho/Controllers/FichaDeDistribuicaoController.cs
+++ Conselho/Controllers/FichaDeDistribuicaoController.cs
@@ -13,56 +13,44 @@ namespace Conselho.Controllers
         // GET: FichaDeDistribuicao
         public ActionResult Index()
         {
-
             MeuContexto contexto = new MeuContexto();
             List<Turma> turmas = contexto.Turmas.ToList();
             return View(turmas);
-
         }
 
 
         public ActionResult AtribuicaoDeAula(int id)
         {
-
             MeuContexto contexto = new MeuContexto();
             DistribuicaoDeAulaViewModel model = new DistribuicaoDeAulaViewModel();
-
             model.ListaDisciplinas = contexto.Disciplinas.ToList();
             model.ListaProfessores = contexto.Professores.ToList();
             model._turma = contexto.Turmas.Find(id);
 
             return View(model);
-
-
-
         }
 
-        [HttpPost]
-        public ActionResult DistribuiAula( int idDisciplina,int idTurma, int idProf)
-            {
-                     MeuContexto contexto = new MeuContexto();
 
-                     List<Disciplina> disciplinas = contexto.Disciplinas.ToList();
-
+        public ActionResult EfetivaDistribuicao(int idTurma, int idProf)
+        {
+            MeuContexto contexto = new MeuContexto();
+            List<Disciplina> disciplinas = contexto.Disciplinas.ToList();
             List<FichaDeDistribuicao> fichas = new List<FichaDeDistribuicao>();
-
 
             return View("Index");
         }
 
-        public ActionResult AdicionarProfessor(int Id)
+        [HttpPost]
+        public ActionResult EfetivaDistribuicao(int idTurma, int idProf, int idDisc)
         {
-
-
             MeuContexto contexto = new MeuContexto();
+            List<Disciplina> disciplinas = contexto.Disciplinas.ToList();
+            List<FichaDeDistribuicao> fichas = new List<FichaDeDistribuicao>();
 
-
-            List<Professor> lista = contexto.Professores.ToList();
-
-
-
-            return View(lista);
+            return View("Index");
         }
+
+
 
     }
 }
