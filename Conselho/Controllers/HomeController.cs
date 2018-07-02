0e4780136d9d4040517562e6dbe1bec3cb8d73c7
MARCELO <marcelofabiano@msn.com>
2018-07-02 12:38:37 -0300

    Reestabelencendo as funcionalidades

--- Conselho/Controllers/HomeController.cs
+++ Conselho/Controllers/HomeController.cs
@@ -9,19 +9,7 @@ namespace Conselho.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-
-            LoginController controle = new LoginController();
-            Professor professor = controle.professorLogado;
-
-            LoginPatialViewModel model = new LoginPatialViewModel();
-
-            model.professor = professor;
-
-            return View(model);
-        }
-
+       
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -36,24 +24,11 @@ namespace Conselho.Controllers
             return View();
         }
 
-        public ActionResult IndexProfessor()
+        public ActionResult Index()
         {
 
             return View();
 
-        }
-
-        public ActionResult _LoginPartialProfessor()
-        {
-
-            LoginController controle = new LoginController();
-            Professor professor = controle.professorLogado;
-
-            LoginPatialViewModel model = new LoginPatialViewModel();
-
-            model.professor = professor;
-
-            return View(model);
         }
 
     }
